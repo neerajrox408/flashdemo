5088cf167146d8310eb3ad63ef2d7712ab48d7f2
neerajrox408 <neerajagrawal765@gmail.com>
2022-07-25 22:29:19 +0530

    done with all files

---
+++ src/test/java/Pushproject/Gitdemopract.java
@@ -0,0 +1,20 @@
+package Pushproject;
+
+public class Gitdemopract {
+
+	public int add () {
+		int a=20;
+		int b=30;
+		int c=a+b;
+	
+		return c;
+
+	}
+	public static void main(String[] args)
+	{
+		Gitdemopract git=new Gitdemopract();
+		git.add();
+		
+	}
+
+}
